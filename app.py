20558ac4356857ed2a1585730033f101aa11a87c
Raghav Sharma <71115825+raghavTinker@users.noreply.github.com>
2023-08-20 18:29:30 +0530

    fix

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def get_bot_response(conversationID):
         conversation.append(
             {"role": "system", "content": "One last thing. If you have got to know the user well, and you have a search_string which I can use to search for products. Format it like this: search_string = \"<search_string>\""}
         )
-        conversation.append({"role": "system", "content": "Format when giving search string is: search_string='<search_string>'"})
+        conversation.append({"role": "system", "content": "Format when giving search string is: search_string='search_string'"})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=conversation,
